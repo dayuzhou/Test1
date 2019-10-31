8e1cdea8cd3b16b22a4f4a8b86b53fda7c0964cf
dayuzhou <2507356905@qq.com>
2019-10-31 13:43:24 +0800

    Initial commit

---
+++ src/com/demo/Main.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i=0;
+	while(true){
+	  System.out.println(i);
+	  if(i==10){
+		  break;
+		}
+		i++;
+    }
+
+    }
+}
